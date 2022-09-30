b00796afcc7aafae85f8cf5dea8c74730c28b95d
moondolph <jysim1000@gmail.com>
2022-09-30 14:32:37 +0900

    9/30
    test commit

---
+++ Spring/Sep30_1_SpringMVC_SpringDI/src/main/java/com/beaver/sep301/fruit/Fruit.java
@@ -0,0 +1,49 @@
+package com.beaver.sep301.fruit;
+
+// 객체를 여러개 등록해서 사용하려면... -> src / main / resources에 xxxbeans.xml 만들어서 했었는데 ....
+
+//singleton 패턴으로 해보자
+//	root - context.xml		: 톰캣에 실려있는 '프로젝트 전체'에 영향 
+//	servlet-context.xml		: 이 프로젝트에 영향 
+//    ㄴ 여기에 객체 등록 
+//	
+
+
+public class Fruit {
+	private String name;
+	private int price;
+	
+	public Fruit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Fruit(String name, int price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
